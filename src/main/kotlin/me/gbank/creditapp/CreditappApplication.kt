2baaed636f3072b5b1a1be654bafe3b54670a9ef
Gabriel Souza <gbsouza.dev@gmail.com>
2023-05-10 09:21:38 -0300

    Create application

---
+++ src/main/kotlin/me/gbank/creditapp/CreditappApplication.kt
@@ -0,0 +1,11 @@
+package me.gbank.creditapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditappApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditappApplication>(*args)
+}
---
+++ src/test/kotlin/me/gbank/creditapp/CreditappApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.gbank.creditapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditappApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
